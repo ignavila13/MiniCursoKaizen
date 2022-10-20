0ecedfb4f349a3659caeb2997467de60feeaf55d
ignavila13 <nachoavil13@gmail.com>
2022-10-20 09:46:53 -0300

    frm employees modificaciones

--- frmEmployees.Designer.cs
+++ frmEmployees.Designer.cs
@@ -37,46 +37,41 @@ namespace MiniCursoKaizen
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(15, 32);
-            this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSearch.Location = new System.Drawing.Point(11, 26);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(303, 22);
+            this.txtSearch.Size = new System.Drawing.Size(228, 20);
             this.txtSearch.TabIndex = 0;
-            this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBuscar_KeyUp);
+            this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Buscar:";
+            this.label1.Text = "Search:";
             // 
             // grdEmployees
             // 
             this.grdEmployees.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.grdEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdEmployees.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.grdEmployees.Location = new System.Drawing.Point(0, 79);
-            this.grdEmployees.Margin = new System.Windows.Forms.Padding(4);
+            this.grdEmployees.Location = new System.Drawing.Point(12, 64);
             this.grdEmployees.Name = "grdEmployees";
             this.grdEmployees.ReadOnly = true;
             this.grdEmployees.RowHeadersWidth = 51;
             this.grdEmployees.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grdEmployees.Size = new System.Drawing.Size(1148, 297);
+            this.grdEmployees.Size = new System.Drawing.Size(837, 230);
             this.grdEmployees.TabIndex = 2;
             // 
             // frmEmployees
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1148, 376);
+            this.ClientSize = new System.Drawing.Size(861, 306);
             this.Controls.Add(this.grdEmployees);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtSearch);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmEmployees";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Employees";
--- frmEmployees.cs
+++ frmEmployees.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿//importo librerias necesarias
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
@@ -7,44 +8,45 @@ namespace MiniCursoKaizen
 {
     public partial class frmEmployees : Form
     {
+        //creo dv global
         DataView dv = new DataView();
-
         public frmEmployees()
         {
-
             InitializeComponent();
-
-            SqlConnection connection = new SqlConnection(ConfigurationManager
-                .ConnectionStrings["conectar"].ConnectionString);
-
+            //conexión a base de datos
+            SqlConnection connection = new SqlConnection();
+            connection = getConnectionDB();
+            //ejecución de procedimiento de almacenado
             SqlCommand qry = new SqlCommand("EXEC spEmployeesSelectAll", connection);
             SqlDataAdapter da = new SqlDataAdapter(qry);
-
+            //asignación de resultados al datatable
             DataTable dt = new DataTable();
             da.Fill(dt);
-            grdEmployees.DataSource = dt.DefaultView;
-
-            dv = dt.DefaultView;
-
-
+            grdEmployees.DataSource = dt.DefaultView; //asignación del dt a la grilla
+            dv = dt.DefaultView; //asignación del dt a dv
         }
-
+        private void txtSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            //el procedimiento será ejecutado cuando el usuario presione una tecla
+            searchEmployee(txtSearch.Text);
+        }
+        private SqlConnection getConnectionDB()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager
+                .ConnectionStrings["conectar"].ConnectionString);
+            return connection;
+        }
+        /*
+         aplicación del filtro por nombre y apellido
+        */
         private void searchEmployee (string filter)
         {
-
             string sqlstring = "";
             dv.RowFilter = sqlstring;
             sqlstring = $"[LastName] LIKE '{filter}%'";
             sqlstring = sqlstring + $" OR [FirstName] LIKE '{filter}%'";
             dv.RowFilter = sqlstring;
-
         }
-
-        private void txtBuscar_KeyUp(object sender, KeyEventArgs e)
-        {
-            searchEmployee(txtSearch.Text);
-        }
-
 
     }
 }
